f0c4a60e7c7061f0511ebce4a5a810404bac64f4
Andrés Grande <andres.grande@somosinapsis.com>
2023-06-07 15:41:25 +0200

    chnage all url to dynamic

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 const baseUrl = `${window.location.protocol}//${window.location.hostname}`;
-console.log(window.location.protocol);
-console.log(window.location.hostname);
+
 /**
  * Simulation: call from IoT device for registering produced energy
  */
@@ -46,7 +45,7 @@ document.getElementById('consumption-form').addEventListener('submit', function(
     const processingElement = document.getElementById('processing2');
     processingElement.style.display = 'block';
 
-    fetch('http://localhost:3000/registerEnergyConsumption', {
+    fetch(`${baseUrl}/registerEnergyConsumption`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -80,7 +79,7 @@ document.getElementById('production-test-form').addEventListener('submit', funct
     // Convert date string into timestamp in seconds
     const timestamp = Math.floor(new Date(timestampInput).getTime() / 1000);
 
-    fetch('http://localhost:3000/registerEnergyProductionTest', {
+    fetch(`${baseUrl}/registerEnergyProductionTest`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -115,7 +114,7 @@ document.getElementById('consumption-test-form').addEventListener('submit', func
     // Convert date string into timestamp in seconds
     const timestamp = Math.floor(new Date(timestampInput).getTime() / 1000);
 
-    fetch('http://localhost:3000/registerEnergyConsumptionTest', {
+    fetch(`${baseUrl}/registerEnergyConsumptionTest`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
