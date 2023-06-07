9894e08e8ab07261cc127db307937acdc4c22dd1
Andrés Grande <andres.grande@somosinapsis.com>
2023-06-08 00:30:39 +0200

    production address

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.static(path.join(__dirname, 'public'))); // Statc files in direc
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_URL));
 
 // Set contract details
-const contractProductionAddress = '0x3b059F2aBd2A677406E04Bf379bF20C92e51CEe5';
-const contractConsumptionAddress = '0xc4364bF88dA24A547658a25386bb49a2c08Ee609'; 
+const contractProductionAddress = '0xf74b7507C29E3eE7453b05E6c086a55cDE12a0F9';
+const contractConsumptionAddress = '0x5f097B1D6811E0948D60b9c8Aa17fCcB98128845'; 
 const contract = new web3.eth.Contract(productionAbi, contractProductionAddress);
 const contractConsumption = new web3.eth.Contract(consumptionAbi, contractConsumptionAddress); // Create a new web3 instance for the consumption contract
 
@@ -131,6 +131,34 @@ app.post('/registerEnergyConsumptionTest', async (req, res) => {
         res.status(400).send(error.toString());
     }
 });
+
+/****************************/
+
+function calculateHourDelay(){
+    const currentTime = new Date();
+    const nextHour = new Date();
+    nextHour.setHours(nextHour.getHours() + 1);
+    nextHour.setMinutes(0);
+    nextHour.setSeconds(0);
+    const delay = nextHour - currentTime;
+    return delay;
+}
+
+function executeEveryHour() {
+    
+    var delay = calculateHourDelay();
+
+    /****/
+     //TODO: call contracto to update energy price every hour
+    /*****/
+
+    // Schedule the next execution at the start of the next hour
+    setTimeout(executeEveryHour, delay);
+}
+  
+  // Calculate initial delay
+  var delay = calculateHourDelay();
+  setTimeout(executeEveryHour, initialDelay);
 
 app.listen(5339, () => console.log('App listening on port 5339!'));
 
